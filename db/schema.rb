1603abc66de878b6263b1c3bd997b7c3cb9ce99c
Hayley Blackstock <Hayley@Hayleys-MacBook-Air.local>
2015-01-22 15:27:34 -0700

    load tables to homepage

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122212828) do
+ActiveRecord::Schema.define(version: 20150122220106) do
+
+  create_table "aliens", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "date_of_birth"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "organizations", force: true do |t|
     t.string   "name"
